a0e9e2448ac468b0efaffe7a73037e269be05592
“marcoviana1610” <marcoviana1610@gmail.com>
2023-09-27 00:33:07 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/EmprestimoController.cs
@@ -0,0 +1,44 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+
+            return View(emprestimos);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid) {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do recebedor!")]
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
+        public string LivroEmprestado { get; set; }
+
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+
+
+    }
+}
